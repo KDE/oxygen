f4669e68493d036fdade0279c4779a30b5389eea
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2013-02-20 17:02:19 +0100

    Fixed handling of minimum widget size when toggling expert button CCBUG: 314764

--- oxygen/kstyle/config/oxygenstyleconfig.h
+++ oxygen/kstyle/config/oxygenstyleconfig.h
@@ -73,6 +73,9 @@ namespace Oxygen
 
         protected Q_SLOTS:
 
+        //! update minimum size
+        void updateMinimumSize( void );
+
         //! update layout
         /*! needed in expert mode to accommodate with animations config widget size changes */
         void updateLayout( void );
@@ -91,6 +94,9 @@ namespace Oxygen
         virtual void toggleExpertModeInternal( bool );
 
         protected:
+
+        //! event processing
+        virtual bool event( QEvent* );
 
         //! load setup from config data
         void load( void );
