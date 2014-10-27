4ad13343c4ccb2188a954b9a7825989ad9f870d2
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2014-10-27 17:40:50 +0100

    fixed dialog titles

--- kwin-decoration/config/oxygenexceptionlistwidget.cpp
+++ kwin-decoration/config/oxygenexceptionlistwidget.cpp
@@ -111,6 +111,7 @@ namespace Oxygen
 
 
         QPointer<ExceptionDialog> dialog = new ExceptionDialog( this );
+        dialog->setWindowTitle( i18n( "New Exception - Oxygen Settings" ) );
         ConfigurationPtr exception( new Configuration() );
 
         #if OXYGEN_USE_KDE4
@@ -163,6 +164,7 @@ namespace Oxygen
 
         // create dialog
         QPointer<ExceptionDialog> dialog( new ExceptionDialog( this ) );
+        dialog->setWindowTitle( i18n( "Edit Exception - Oxygen Settings" ) );
         dialog->setException( exception );
 
         // map dialog
@@ -195,7 +197,7 @@ namespace Oxygen
 
         // confirmation dialog
         {
-            QMessageBox messageBox( QMessageBox::Question, i18n("Question - Breeze Settings" ), i18n("Remove selected exception?"), QMessageBox::Yes | QMessageBox::Cancel );
+            QMessageBox messageBox( QMessageBox::Question, i18n("Question - Oxygen Settings" ), i18n("Remove selected exception?"), QMessageBox::Yes | QMessageBox::Cancel );
             messageBox.button( QMessageBox::Yes )->setText( i18n("Remove") );
             messageBox.setDefaultButton( QMessageBox::Cancel );
             if( messageBox.exec() == QMessageBox::Cancel ) return;
