363b9d20eaff9e0da7ed6c01c1bd5dc39d960e00
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2015-04-24 10:00:30 +0200

    Trigger update directly, on active state changed, when animations are disabled.

--- kdecoration/oxygendecoration.cpp
+++ kdecoration/oxygendecoration.cpp
@@ -195,8 +195,14 @@ namespace Oxygen
     {
         if( m_internalSettings->animationsEnabled() )
         {
+
             m_animation->setDirection( client().data()->isActive() ? QPropertyAnimation::Forward : QPropertyAnimation::Backward );
             if( m_animation->state() != QPropertyAnimation::Running ) m_animation->start();
+
+        } else {
+
+            update();
+
         }
     }
 
