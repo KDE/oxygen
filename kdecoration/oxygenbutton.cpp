f99a3fde2b6f92ccc2d41140517b4e3e0bc1ebb1
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2015-05-05 22:11:09 +0200

    make sure iconSize is valid before rendering

--- kdecoration/oxygenbutton.cpp
+++ kdecoration/oxygenbutton.cpp
@@ -169,6 +169,8 @@ namespace Oxygen
         if( m_flag == FlagFirstInList ) painter->translate( m_offset );
         else painter->translate( 0, m_offset.y() );
 
+        if( !m_iconSize.isValid() ) m_iconSize = geometry().size().toSize();
+
         // menu buttons
         if( isMenuButton() )
         {
