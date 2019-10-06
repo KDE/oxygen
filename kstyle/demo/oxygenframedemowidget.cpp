ad3cddb5954ec4e7d15323b648fd7433ddc8770f
René Bertin <rjvbertin@gmail.com>
2019-10-06 16:24:17 +0200

    Fix the KDE4 build of the oxygen-demo
    Was broken by my previous commit introducing a KMessageWidget preview.
    
    Differential Revision: https://phabricator.kde.org/D13881

--- kstyle/demo/oxygenframedemowidget.cpp
+++ kstyle/demo/oxygenframedemowidget.cpp
@@ -25,12 +25,15 @@
 //////////////////////////////////////////////////////////////////////////////
 
 #include "oxygenframedemowidget.h"
+#include "config-liboxygen.h"
 
 #include <QButtonGroup>
 
 #include <KComboBox>
 #include <KMessageWidget>
+#if !OXYGEN_USE_KDE4
 #include <kwidgetsaddons_version.h>
+#endif
 
 namespace Oxygen
 {
@@ -58,7 +61,7 @@ namespace Oxygen
         connect( ui.flatGroupBoxCheckBox, SIGNAL(toggled(bool)), SLOT(toggleFlatGroupBox(bool)) );
 
         addMessages();
-#if KWIDGETSADDONS_VERSION < QT_VERSION_CHECK(5, 48, 0)
+#if !OXYGEN_USE_KDE4 && KWIDGETSADDONS_VERSION < QT_VERSION_CHECK(5, 48, 0)
         qApp->installEventFilter(this);
 #endif
     }
@@ -99,7 +102,7 @@ namespace Oxygen
 
     bool FrameDemoWidget::eventFilter( QObject *obj, QEvent *event )
     {
-#if KWIDGETSADDONS_VERSION < QT_VERSION_CHECK(5, 48, 0)
+#if !OXYGEN_USE_KDE4 && KWIDGETSADDONS_VERSION < QT_VERSION_CHECK(5, 48, 0)
         if (event->type() == QEvent::DynamicPropertyChange && obj == qApp) {
             QDynamicPropertyChangeEvent *e = dynamic_cast<QDynamicPropertyChangeEvent*>(event);
             if (e->propertyName() == QByteArrayLiteral("KDE_COLOR_SCHEME_PATH")) {
