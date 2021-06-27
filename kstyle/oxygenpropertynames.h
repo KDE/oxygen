40066e3d1636429b9bade4d78e687a2a09d4612d
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-06-27 19:26:13 +0200

    Convert remaining copyright texts to SPDX

--- kstyle/oxygenpropertynames.h
+++ kstyle/oxygenpropertynames.h
@@ -7,24 +7,10 @@
 * -------------------
 *
 * SPDX-FileCopyrightText: 2012 Hugo Pereira Da Costa <hugo.pereira@free.fr>
+* SPDX-FileCopyrightText: 2003 - 2010 Craig Drummond <craig.p.drummond@gmail.com>
+* SPDX-License-Identifier: LGPL-2.0-or-later
 *
 * Largely inspired from Qtcurve style
-* Copyright (C) Craig Drummond, 2003 - 2010 craig.p.drummond@gmail.com
-*
-* This  library is free  software; you can  redistribute it and/or
-* modify it  under  the terms  of the  GNU Lesser  General  Public
-* License  as published  by the Free  Software  Foundation; either
-* version 2 of the License, or( at your option ) any later version.
-*
-* This library is distributed  in the hope that it will be useful,
-* but  WITHOUT ANY WARRANTY; without even  the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License  along  with  this library;  if not,  write to  the Free
-* Software Foundation, Inc., 51  Franklin St, Fifth Floor, Boston,
-* MA 02110-1301, USA.
 */
 
 namespace Oxygen
