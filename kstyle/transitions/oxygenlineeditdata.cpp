3b4a2b8062175332c9f6b65556f2089e72b90718
Laurent Montel <montel@kde.org>
2023-06-15 08:40:57 +0200

    valueChanged was deprecated in qt5 and removed in qt6 => use textChanged

--- kstyle/transitions/oxygenlineeditdata.cpp
+++ kstyle/transitions/oxygenlineeditdata.cpp
@@ -51,7 +51,7 @@ LineEditData::LineEditData(QObject *parent, QLineEdit *target, int duration)
     QLineEdit
     */
     if (qobject_cast<QSpinBox *>(_target.data()->parentWidget()) || qobject_cast<QDoubleSpinBox *>(_target.data()->parentWidget())) {
-        connect(_target.data()->parentWidget(), SIGNAL(valueChanged(QString)), SLOT(textChanged()));
+        connect(_target.data()->parentWidget(), SIGNAL(textChanged(QString)), SLOT(textChanged()));
 
     } else if (qobject_cast<QDateTimeEdit *>(_target.data()->parentWidget())) {
         connect(_target.data()->parentWidget(), SIGNAL(dateTimeChanged(QDateTime)), SLOT(textChanged()));
